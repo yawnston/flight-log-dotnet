6534af9ab30148594fb0c07ad734fbfe43a41b66
Daniel Crha <d.crha@kendaxa.com>
2020-05-13 12:55:10 +0200

    Add basic Selenium frontend tests

---
+++ SelNet/Program.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace SelNet
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        [Fact]
+        public void TestCreation()
+        {
+            using SeleniumTest sel = new SeleniumTest();
+            ChromeDriver driver = sel.driver;
+
+            Debug.WriteLine($"Title of page is: {driver.Title}");
+            int rowCount = driver.FindElements(By.XPath("/html/body/router-view/div/div/div/table/tbody/tr")).Count;
+            IWebElement novyLet = driver.FindElement(By.XPath("/html/body/div[1]/div/ul/li[2]/a"));
+            novyLet.Click();
+
+            driver.FindElement(By.XPath("//*[@id=\"takeoffTime\"]")).SendKeys("12120020181956");
+            driver.FindElement(By.XPath("/html/body/router-view/div/form/div[2]/div/button")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+            wait.Until(d =>
+            {
+                try
+                {
+                    return d.SwitchTo().Alert();
+                }
+                catch (NoAlertPresentException)
+                {
+                    return null;
+                }
+            }).Accept();
+
+            driver.FindElement(By.XPath("/html/body/div[1]/div/ul/li[1]/a")).Click();
+            int newCount = driver.FindElements(By.XPath("/html/body/router-view/div/div/div/table/tbody/tr")).Count;
+            Assert.Equal(rowCount + 2, newCount);
+        }
+    }
+}
---
+++ SelNet/SeleniumTest.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SelNet
+{
+    internal class SeleniumTest : IDisposable
+    {
+        public readonly ChromeDriver driver;
+
+        public SeleniumTest()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://localhost:44313/";
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
